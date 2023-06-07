33e0818e3f0cb275999528a9dcc5514b2dbe609f
Nazmul Hussain <nazmul.hussain.utchash@gmail.com>
2023-06-07 14:13:36 +0600

    complete

--- src/Pages/Booking/Booking.jsx
+++ src/Pages/Booking/Booking.jsx
@@ -1,25 +1,43 @@
+import { useEffect, useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 
 const Booking = () => {
     const showDetails = useLoaderData()
-    console.log(showDetails);
+
+    const handleBooking = (event) => {
+        event.preventDefault()
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+
+        const bookingData = {
+            name,
+            email,
+            moviesId: [ showDetails.id ]
+        }
+
+        const bookingDataString = JSON.stringify(bookingData)
+
+        localStorage.setItem('bookingData', (bookingDataString))
+    }
+
     return (
         <div>
             <h2 className="text-3xl font-bold text-center my-20">Book Movie</h2>
-            <div>
+            <form onSubmit={handleBooking} >
                 <div className="flex gap-2">
                     <div className="form-control w-full max-w-xs">
                         <label className="label">
                             <span className="label-text">Name*</span>
                         </label>
-                        <input type="text" placeholder="Name" className="input input-bordered w-full max-w-xs" required />
+                        <input type="text" placeholder="Name" name="name" className="input input-bordered w-full max-w-xs" required />
                     </div>
                     <div className="form-control w-full">
                         <label className="label">
                             <span className="label-text">Email*</span>
                         </label>
-                        <input type="email" placeholder="Email" className="input input-bordered w-full " required />
+                        <input type="email" placeholder="Email" name="email" className="input input-bordered w-full " required />
                     </div>
                 </div>
                 <div className="form-control w-full">
@@ -39,13 +57,13 @@ const Booking = () => {
                         <label className="label">
                             <span className="label-text">Time</span>
                         </label>
-                        <input type="time"  className="input input-bordered w-full " required />
+                        <input type="time" className="input input-bordered w-full " required />
                     </div>
                 </div>
                 <div className="flex justify-center my-12">
-                    <button className="btn btn-primary">Confirm Booking</button>
+                    <input className="btn btn-primary" type="submit" value="Confirm Booking" />
                 </div>
-            </div>
+            </form>
         </div>
     );
 };
